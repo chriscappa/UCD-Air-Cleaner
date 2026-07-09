b6b9e3c5ff0d4ab24a0a8bcc1bcedfc9ecb7496f
chriscappa <cdcappa@ucdavis.edu>
2026-07-09 14:54:02 -0700

    Update PMS5003_Particle.h

--- src/drivers/PMS5003_Particle.h
+++ src/drivers/PMS5003_Particle.h
@@ -8,8 +8,20 @@ class PMS5003_Particle {
 public:
     PMS5003_Particle();
     void begin();
-    bool readData(uint16_t &pm1_0, uint16_t &pm2_5, uint16_t &pm10, uint16_t &particles_0_3);
+    
+    // Aligns perfectly with structural telemetry mapping tasks inside main.cpp
+    uint16_t getPM1_0() const;
+    uint16_t getPM2_5() const;
+    uint16_t getPM10() const;
+    uint16_t getRawParticleCount() const;
 
 private:
+    static void parseUartTask(void* pvParameters);
+    
     HardwareSerial _pmsSerial;
+    
+    uint16_t _pm1_0;
+    uint16_t _pm2_5;
+    uint16_t _pm10;
+    uint16_t _particleCount;
 };
